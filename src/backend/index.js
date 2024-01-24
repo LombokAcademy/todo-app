8051c5f4a96200c0d98f54a579fa1ffba3070086
juhan26 <cukjuhan@gmail.com>
2024-01-24 18:36:01 +0800

    Install body-parser & cors, and update the API

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,47 +1,50 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// untuk sampel data nya 
-let users = [
-  { id: 1, username: 'user1' },
-  { id: 2, username: 'user2' },
+// sample datanya
+let items = [
+  { id: 1, name: 'Item 1' },
+  { id: 2, name: 'Item 2' },
 ];
 
-app.use(express.json());
+// mid ware
+app.use(bodyParser.json());
+app.use(cors());
 
 // route nya
 app.get('/', (req, res) => {
-  res.send('User API nya sudah berjalan!');
+  res.send('ini express api nya sudah berjalan!');
 });
 
-// mengambil semua
-app.get('/users', (req, res) => {
-  res.json(users);
+// mengambil semua items
+app.get('/items', (req, res) => {
+  res.json(items);
 });
 
-// get spesifikasi user by id
-app.get('/users/:id', (req, res) => {
-  const userId = parseInt(req.params.id);
-  const user = users.find((user) => user.id === userId);
+// mengambil specific item dari id
+app.get('/items/:id', (req, res) => {
+  const itemId = parseInt(req.params.id);
+  const item = items.find((item) => item.id === itemId);
 
-  if (user) {
-    res.json(user);
+  if (item) {
+    res.json(item);
   } else {
-    res.status(404).json({ error: 'User not found' });
+    res.status(404).json({ error: 'Item not found' });
   }
 });
 
-// menambahkan user baru
-app.post('/users', (req, res) => {
-  const newUser = req.body;
-  users.push(newUser);
-  res.status(201).json(newUser);
+// add a new item
+app.post('/items', (req, res) => {
+  const newItem = req.body;
+  items.push(newItem);
+  res.status(201).json(newItem);
 });
 
-
-// start servernya
+// start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
