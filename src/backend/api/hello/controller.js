9aca9212fb3350a38c279140878f1a441d376700
juhan26 <cukjuhan@gmail.com>
2024-01-28 11:13:15 +0800

    create /api/hello endpoint

---
+++ src/backend/api/hello/controller.js
@@ -0,0 +1,14 @@
+import { hello } from "./index.js";
+
+const get = async(req,res,next) => {
+    try {
+        const result = await hello()
+        res.status(200).json({
+            message : result
+        })
+    } catch (error) {
+        next(error)
+    }
+
+}
+export default{get}
