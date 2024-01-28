49c2439210afa10e221d9a5e61b82e354410e7a1
juhan26 <cukjuhan@gmail.com>
2024-01-28 18:38:38 +0800

    add back some codes that has been removed

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,11 +1,37 @@
-const express = require('express');
+require('dotenv').config()
+const express = require("express");
+const http = require("http");
+const path = require("path");
+
+const API_PORT = process.env.API_PORT || 3000;
 const app = express();
+
+// setup static content from vite build output dir in dist/ui if NODE_ENV is in development mode
+let STATIC_PATH = "../../dist/ui";
+if (process.env.NODE_ENV !== "production") {
+  STATIC_PATH = path.join(__dirname, "../../dist/ui");
+} else {
+  STATIC_PATH = path.join(__dirname, "ui");
+}
+
+app.use(express.static(STATIC_PATH));
+
+// Setup Express routes here before starting the server
+// initial
 
 const helloRoute = require('./api/hello/route');
 
 app.use('/api/hello', helloRoute);
+// serve all requests to index.html in production
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(STATIC_PATH, "index.html"));
+  });
+}
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server berjalan di http://localhost:${PORT}`);
+
+// start server
+const server = http.createServer(app);
+server.listen(API_PORT, () => {
+  console.log(`Server listening on port ${API_PORT}`);
 });
