410a60588059edfe4ae1ac3d594880ded1a3a70c
Chandra Kusuma <77940961+CzNime@users.noreply.github.com>
2024-02-22 12:53:09 +0800

    creating task model (#19)
    * Setup Sequelize for mysql database
    
    * remove unused code
    
    * create task model
    
    * change datatype to UUID
    
    * formatting document
    
    ---------
    
    Co-authored-by: BangCan <chandrakusuma13@gmail.com>

---
+++ src/backend/database/task.model.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("./connection.js");
+
+const Task = sequelize.define("Task", {
+  id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4,
+    unique: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  status: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = Task;
